293a648acfa17bba36d58008254e8532c07a367d
michael.stattmann@gmail.com <michael.stattmann@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-05-12 21:08:58 +0000

    some cleanup and added dummy impl for AppleWaveBox. mp4s is not audio but systems. meta box parsing doesn't work.
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@125 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ src/main/java/com/coremedia/iso/boxes/apple/AppleWaveBox.java
@@ -0,0 +1,31 @@
+package com.coremedia.iso.boxes.apple;
+
+import com.coremedia.iso.BoxParser;
+import com.coremedia.iso.IsoBufferWrapper;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.boxes.AbstractContainerBox;
+import com.coremedia.iso.boxes.Box;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public final class AppleWaveBox extends AbstractContainerBox {
+  public static final String TYPE = "wave";
+
+  public AppleWaveBox() {
+    super(IsoFile.fourCCtoBytes(TYPE));
+  }
+
+  @Override
+  public void parse(IsoBufferWrapper in, long size, BoxParser boxParser, Box lastMovieFragmentBox) throws IOException {
+    super.parse(in, size, boxParser, lastMovieFragmentBox);
+  }
+
+  @Override
+  public String getDisplayName() {
+    return "iTunes Proprietary Wave Box";
+  }
+
+}
