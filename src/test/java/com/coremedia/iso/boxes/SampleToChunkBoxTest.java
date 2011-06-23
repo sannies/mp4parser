984b2db195f9c36223a5aa4c5ed7561a0c7303c4
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-06-23 14:53:32 +0000

    added test
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@146 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ src/test/java/com/coremedia/iso/boxes/SampleToChunkBoxTest.java
@@ -0,0 +1,44 @@
+package com.coremedia.iso.boxes;
+
+import com.coremedia.iso.IsoBufferWrapper;
+import com.coremedia.iso.IsoOutputStream;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sannies
+ * Date: 24.02.11
+ * Time: 12:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleToChunkBoxTest extends TestCase {
+
+
+    public void testParse() throws Exception {
+        SampleToChunkBox stsc = new SampleToChunkBox();
+        List<SampleToChunkBox.Entry> l = new LinkedList<SampleToChunkBox.Entry>();
+        for (int i = 0; i < 5; i++) {
+            SampleToChunkBox.Entry e = new SampleToChunkBox.Entry();
+            e.setFirstChunk(i);
+            e.setSampleDescriptionIndex(1);
+            e.setSamplesPerChunk(i * i);
+            l.add(e);
+        }
+        stsc.setEntries(l);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        stsc.getBox(new IsoOutputStream(baos));
+        byte[] content = baos.toByteArray();
+
+        SampleToChunkBox stsc2 = new SampleToChunkBox();
+        stsc2.parse(new IsoBufferWrapper(ByteBuffer.wrap(content, 8, content.length - 8)), content.length + 8, null, null);
+        Assert.assertEquals(content.length, stsc2.getSize());
+
+
+    }
+}
