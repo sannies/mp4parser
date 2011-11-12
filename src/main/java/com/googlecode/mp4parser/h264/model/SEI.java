836980ebfad6428cec2395633a728821b271db1f
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-11-12 09:57:09 +0000

    Add constants for SEI message types
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@256 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/h264/model/SEI.java
+++ src/main/java/com/googlecode/mp4parser/h264/model/SEI.java
@@ -28,6 +28,7 @@ import com.googlecode.mp4parser.h264.write.CAVLCWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Supplementary Enhanced Information entity of H264 bitstream
@@ -37,6 +38,53 @@ import java.util.ArrayList;
  * @author Stanislav Vitvitskiy
  */
 public class SEI extends BitstreamElement {
+
+    public static final int BUFFERING_PERIOD = 0;
+    public static final int PIC_TIMING = 1;
+    public static final int PAN_SCAN_RECT = 2;
+    public static final int FILLER_PAYLOAD = 3;
+    public static final int USER_DATA_REGISTERED_ITU_T_T35 = 4;
+    public static final int USER_DATA_UNREGISTERED = 5;
+    public static final int RECOVERY_POINT = 6;
+    public static final int DEC_REF_PIC_MARKING_REPETITION = 7;
+    public static final int SPARE_PIC = 8;
+    public static final int SCENE_INFO = 9;
+    public static final int SUB_SEQ_INFO = 10;
+    public static final int SUB_SEQ_LAYER_CHARACTERISTICS = 11;
+    public static final int SUB_SEQ_CHARACTERISTICS = 12;
+    public static final int FULL_FRAME_FREEZE = 13;
+    public static final int FULL_FRAME_FREEZE_RELEASE = 14;
+    public static final int FULL_FRAME_SNAPSHOT = 15;
+    public static final int PROGRESSIVE_REFINEMENT_SEGMENT_START = 16;
+    public static final int PROGRESSIVE_REFINEMENT_SEGMENT_END = 17;
+    public static final int MOTION_CONSTRAINED_SLICE_GROUP_SET = 18;
+    public static final int FILM_GRAIN_CHARACTERISTICS = 19;
+    public static final int DEBLOCKING_FILTER_DISPLAY_PREFERENCE = 20;
+    public static final int STEREO_VIDEO_INFO = 21;
+    public static final int POST_FILTER_HINT = 22;
+    public static final int TONE_MAPPING_INFO = 23;
+    public static final int SCALABILITY_INFO = 24;
+    public static final int SUB_PIC_SCALABLE_LAYER = 25;
+    public static final int NON_REQUIRED_LAYER_REP = 26;
+    public static final int PRIORITY_LAYER_INFO = 27;
+    public static final int LAYERS_NOT_PRESENT = 28;
+    public static final int LAYER_DEPENDENCY_CHANGE = 29;
+    public static final int SCALABLE_NESTING = 30;
+    public static final int BASE_LAYER_TEMPORAL_HRD = 31;
+    public static final int QUALITY_LAYER_INTEGRITY_CHECK = 32;
+    public static final int REDUNDANT_PIC_PROPERTY = 33;
+    public static final int TL0_DEP_REP_INDEX = 34;
+    public static final int TL_SWITCHING_POINT = 35;
+    public static final int PARALLEL_DECODING_INFO = 36;
+    public static final int MVC_SCALABLE_NESTING = 37;
+    public static final int VIEW_SCALABILITY_INFO = 38;
+    public static final int MULTIVIEW_SCENE_INFO = 39;
+    public static final int MULTIVIEW_ACQUISITION_INFO = 40;
+    public static final int NON_REQUIRED_VIEW_COMPONENT = 41;
+    public static final int VIEW_DEPENDENCY_CHANGE = 42;
+    public static final int OPERATION_POINTS_NOT_PRESENT = 43;
+    public static final int BASE_VIEW_TEMPORAL_HRD = 44;
+    public static final int FRAME_PACKING_ARRANGEMENT = 45;
 
     public static class SEIMessage {
         public int payloadType;
@@ -105,5 +153,15 @@ public class SEI extends BitstreamElement {
 
         writer.writeTrailingBits();
 
+    }
+
+    @Override
+    public String toString() {
+        String messagesString = "";
+        for (SEIMessage message : messages) {
+            messagesString += SEIMessageToStringer.toString(message) + ", ";
+        }
+        return "SEI{" +
+                "messages=" + messagesString + '}';
     }
 }
