db6f07c4cfa5d9891ee6d6799b77bb68f2567cc2
michael.stattmann@gmail.com <michael.stattmann@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-11-12 07:33:01 +0000

    ItemDataBox
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@255 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ src/main/java/com/coremedia/iso/boxes/ItemDataBox.java
@@ -0,0 +1,49 @@
+package com.coremedia.iso.boxes;
+
+import com.coremedia.iso.BoxParser;
+import com.coremedia.iso.IsoBufferWrapper;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.IsoOutputStream;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class ItemDataBox extends AbstractFullBox {
+    byte[] data;
+    public static final String TYPE = "idat";
+
+    public ItemDataBox() {
+        super(IsoFile.fourCCtoBytes(TYPE));
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    @Override
+    protected long getContentSize() {
+        return data.length;
+    }
+
+    @Override
+    protected void getContent(IsoOutputStream os) throws IOException {
+        os.write(data);
+    }
+
+    @Override
+    public void parse(IsoBufferWrapper in, long size, BoxParser boxParser, Box lastMovieFragmentBox) throws IOException {
+        long a = in.remaining();
+        super.parse(in, size, boxParser, lastMovieFragmentBox);
+        long b = in.remaining();
+        size -= (a - b);
+        assert size < Integer.MAX_VALUE;
+        data = new byte[(int) size];
+        in.read(data);
+    }
+}
