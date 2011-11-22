cea0e72f29a340bfa321d0d14ab52f6ef39c5e7b
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-11-22 17:15:34 +0000

    added SampleEncryptionBox as specified by ISO
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@274 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ src/main/java/com/googlecode/mp4parser/boxes/AbstractSampleEncryptionBox.java
@@ -0,0 +1,231 @@
+package com.googlecode.mp4parser.boxes;
+
+import com.coremedia.iso.BoxParser;
+import com.coremedia.iso.Hex;
+import com.coremedia.iso.IsoBufferWrapper;
+import com.coremedia.iso.IsoOutputStream;
+import com.coremedia.iso.boxes.AbstractFullBox;
+import com.coremedia.iso.boxes.Box;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public abstract class AbstractSampleEncryptionBox  extends AbstractFullBox {
+    int algorithmId = -1;
+    int ivSize = -1;
+    byte[] kid = null;
+    List<Entry> entries = new LinkedList<Entry>();
+
+    protected AbstractSampleEncryptionBox(String type) {
+        super(type);
+    }
+
+
+    @Override
+    protected void getContent(IsoOutputStream os) throws IOException {
+        if (isOverrideTrackEncryptionBoxParameters()) {
+            os.writeUInt24(algorithmId);
+            os.writeUInt8(ivSize);
+            os.write(kid);
+        }
+        os.writeUInt32(entries.size());
+        for (Entry entry : entries) {
+            os.write(entry.iv);
+            if (isSubSampleEncryption()) {
+                os.writeUInt16(entry.pairs.size());
+                for (Entry.Pair pair : entry.pairs) {
+                    os.writeUInt16(pair.clear);
+                    os.writeUInt32(pair.encrypted);
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void parse(IsoBufferWrapper in, long size, BoxParser boxParser, Box lastMovieFragmentBox) throws IOException {
+        super.parse(in, size, boxParser, lastMovieFragmentBox);
+        if ((getFlags() & 0x1) > 0) {
+            algorithmId = in.readUInt24();
+            ivSize = in.readUInt8();
+            kid = in.read(16);
+        }
+        long numOfEntries = in.readUInt32();
+        while (numOfEntries-- > 0) {
+            Entry e = new Entry();
+            e.iv = in.read(((getFlags() & 0x1) > 0) ? ivSize : 8);
+            if ((getFlags() & 0x2) > 0) {
+                int numOfPairs = in.readUInt16();
+                while (numOfPairs-- > 0) {
+                    e.pairs.add(new Entry.Pair(in.readUInt16(), in.readUInt32()));
+                }
+            }
+            entries.add(e);
+
+        }
+    }
+
+
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public int getAlgorithmId() {
+        return algorithmId;
+    }
+
+    public void setAlgorithmId(int algorithmId) {
+        this.algorithmId = algorithmId;
+    }
+
+    public int getIvSize() {
+        return ivSize;
+    }
+
+    public void setIvSize(int ivSize) {
+        this.ivSize = ivSize;
+    }
+
+    public byte[] getKid() {
+        return kid;
+    }
+
+    public void setKid(byte[] kid) {
+        this.kid = kid;
+    }
+
+
+    public boolean isSubSampleEncryption() {
+        return (entries.get(0).pairs.size() > 0);
+    }
+
+    public boolean isOverrideTrackEncryptionBoxParameters() {
+        return kid != null && algorithmId > 0 && ivSize > 0;
+    }
+
+    @Override
+    protected long getContentSize() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            getContent(new IsoOutputStream(baos));
+        } catch (IOException e) {
+            return -1;
+        }
+        return baos.toByteArray().length;
+    }
+
+    @Override
+    public void getBox(IsoOutputStream os) throws IOException {
+        setFlags(0x0);
+        if (isOverrideTrackEncryptionBoxParameters()) {
+            setFlags(getFlags() | 0x1);
+        }
+        if (isSubSampleEncryption()) {
+            setFlags(getFlags() | 0x2);
+        }
+
+        super.getBox(os);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public static class Entry {
+        public byte[] iv;
+        public List<Pair> pairs = new LinkedList<Pair>();
+
+        public static class Pair {
+            public int clear;
+            public long encrypted;
+
+            public Pair(int clear, long encrypted) {
+                this.clear = clear;
+                this.encrypted = encrypted;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                Pair pair = (Pair) o;
+
+                if (clear != pair.clear) return false;
+                if (encrypted != pair.encrypted) return false;
+
+                return true;
+            }
+
+            @Override
+            public int hashCode() {
+                int result = clear;
+                result = 31 * result + (int) (encrypted ^ (encrypted >>> 32));
+                return result;
+            }
+
+            @Override
+            public String toString() {
+                return "clr:" + clear + " enc:" + encrypted;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Entry entry = (Entry) o;
+
+            if (!Arrays.equals(iv, entry.iv)) return false;
+            if (pairs != null ? !pairs.equals(entry.pairs) : entry.pairs != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = iv != null ? Arrays.hashCode(iv) : 0;
+            result = 31 * result + (pairs != null ? pairs.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "iv=" + Hex.encodeHex(iv) +
+                    ", pairs=" + pairs +
+                    '}';
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractSampleEncryptionBox that = (AbstractSampleEncryptionBox) o;
+
+        if (algorithmId != that.algorithmId) return false;
+        if (ivSize != that.ivSize) return false;
+        if (entries != null ? !entries.equals(that.entries) : that.entries != null) return false;
+        if (!Arrays.equals(kid, that.kid)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = algorithmId;
+        result = 31 * result + ivSize;
+        result = 31 * result + (kid != null ? Arrays.hashCode(kid) : 0);
+        result = 31 * result + (entries != null ? entries.hashCode() : 0);
+        return result;
+    }
+}
--- src/main/java/com/googlecode/mp4parser/boxes/piff/PiffSampleEncryptionBox.java
+++ src/main/java/com/googlecode/mp4parser/boxes/piff/PiffSampleEncryptionBox.java
@@ -3,6 +3,7 @@ package com.googlecode.mp4parser.boxes.piff;
 import com.coremedia.iso.*;
 import com.coremedia.iso.boxes.AbstractFullBox;
 import com.coremedia.iso.boxes.Box;
+import com.googlecode.mp4parser.boxes.AbstractSampleEncryptionBox;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -11,35 +12,38 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * <pre>
  * aligned(8) class SampleEncryptionBox extends FullBox(‘uuid’, extended_type= 0xA2394F52-5A9B-4f14-A244-6C427C648DF4, version=0, flags=0)
  * {
- * <p/>
- * unsigned int (32) sample_count;
- * {
- * unsigned int(16) InitializationVector;
- * }[ sample_count ]
+ *  if (flags & 0x000001)
+ *  {
+ *   unsigned int(24) AlgorithmID;
+ *   unsigned int(8) IV_size;
+ *   unsigned int(8)[16] KID;
+ *  }
+ *  unsigned int (32) sample_count;
+ *  {
+ *   unsigned int(IV_size) InitializationVector;
+ *   if (flags & 0x000002)
+ *   {
+ *    unsigned int(16) NumberOfEntries;
+ *    {
+ *     unsigned int(16) BytesOfClearData;
+ *     unsigned int(32) BytesOfEncryptedData;
+ *    } [ NumberOfEntries]
+ *   }
+ *  }[ sample_count ]
  * }
+ * </pre>
  */
-public class PiffSampleEncryptionBox extends AbstractFullBox {
-    int algorithmId = -1;
-    int ivSize = -1;
-    byte[] kid = null;
-    List<Entry> entries = new LinkedList<Entry>();
+public class PiffSampleEncryptionBox extends AbstractSampleEncryptionBox {
 
     /**
-     * Creates a SampleEncryptionBox for non-h264 tracks.
+     * Creates a AbstractSampleEncryptionBox for non-h264 tracks.
      */
     public PiffSampleEncryptionBox() {
-        super(IsoFile.fourCCtoBytes("uuid"));
+        super("uuid");
 
-    }
-
-    public List<Entry> getEntries() {
-        return entries;
-    }
-
-    public void setEntries(List<Entry> entries) {
-        this.entries = entries;
     }
 
     @Override
@@ -47,196 +51,4 @@ public class PiffSampleEncryptionBox extends AbstractFullBox {
         return new byte[]{(byte) 0xA2, 0x39, 0x4F, 0x52, 0x5A, (byte) 0x9B, 0x4f, 0x14, (byte) 0xA2, 0x44, 0x6C, 0x42, 0x7C, 0x64, (byte) 0x8D, (byte) 0xF4};
     }
 
-
-    @Override
-    protected long getContentSize() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            getContent(new IsoOutputStream(baos));
-        } catch (IOException e) {
-            return -1;
-        }
-        return baos.toByteArray().length;
-    }
-
-    public boolean isSubSampleEncryption() {
-        return (entries.get(0).pairs.size() > 0);
-    }
-
-    public boolean isOverrideTrackEncryptionBoxParameters() {
-        return kid != null && algorithmId > 0 && ivSize > 0;
-    }
-
-    public int getAlgorithmId() {
-        return algorithmId;
-    }
-
-    public void setAlgorithmId(int algorithmId) {
-        this.algorithmId = algorithmId;
-    }
-
-    public int getIvSize() {
-        return ivSize;
-    }
-
-    public void setIvSize(int ivSize) {
-        this.ivSize = ivSize;
-    }
-
-    public byte[] getKid() {
-        return kid;
-    }
-
-    public void setKid(byte[] kid) {
-        this.kid = kid;
-    }
-
-    @Override
-    public void getBox(IsoOutputStream os) throws IOException {
-        setFlags(0x0);
-        if (isOverrideTrackEncryptionBoxParameters()) {
-            setFlags(getFlags() | 0x1);
-        }
-        if (isSubSampleEncryption()) {
-            setFlags(getFlags() | 0x2);
-        }
-
-        super.getBox(os);    //To change body of overridden methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void parse(IsoBufferWrapper in, long size, BoxParser boxParser, Box lastMovieFragmentBox) throws IOException {
-        super.parse(in, size, boxParser, lastMovieFragmentBox);
-        if ((getFlags() & 0x1) > 0) {
-            algorithmId = in.readUInt24();
-            ivSize = in.readUInt8();
-            kid = in.read(16);
-        }
-        long numOfEntries = in.readUInt32();
-        while (numOfEntries-- > 0) {
-            Entry e = new Entry();
-            e.iv = in.read(((getFlags() & 0x1) > 0) ? ivSize : 8);
-            if ((getFlags() & 0x2) > 0) {
-                int numOfPairs = in.readUInt16();
-                while (numOfPairs-- > 0) {
-                    e.pairs.add(new Entry.Pair(in.readUInt16(), in.readUInt32()));
-                }
-            }
-            entries.add(e);
-
-        }
-    }
-
-    @Override
-    protected void getContent(IsoOutputStream os) throws IOException {
-        if (isOverrideTrackEncryptionBoxParameters()) {
-            os.writeUInt24(algorithmId);
-            os.writeUInt8(ivSize);
-            os.write(kid);
-        }
-        os.writeUInt32(entries.size());
-        for (Entry entry : entries) {
-            os.write(entry.iv);
-            if (isSubSampleEncryption()) {
-                os.writeUInt16(entry.pairs.size());
-                for (Entry.Pair pair : entry.pairs) {
-                    os.writeUInt16(pair.clear);
-                    os.writeUInt32(pair.encrypted);
-                }
-            }
-        }
-    }
-
-
-    public static class Entry {
-        public byte[] iv;
-        public List<Pair> pairs = new LinkedList<Pair>();
-
-        public static class Pair {
-            public int clear;
-            public long encrypted;
-
-            public Pair(int clear, long encrypted) {
-                this.clear = clear;
-                this.encrypted = encrypted;
-            }
-
-            @Override
-            public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
-
-                Pair pair = (Pair) o;
-
-                if (clear != pair.clear) return false;
-                if (encrypted != pair.encrypted) return false;
-
-                return true;
-            }
-
-            @Override
-            public int hashCode() {
-                int result = clear;
-                result = 31 * result + (int) (encrypted ^ (encrypted >>> 32));
-                return result;
-            }
-
-            @Override
-            public String toString() {
-                return "clr:" + clear + " enc:" + encrypted;
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Entry entry = (Entry) o;
-
-            if (!Arrays.equals(iv, entry.iv)) return false;
-            if (pairs != null ? !pairs.equals(entry.pairs) : entry.pairs != null) return false;
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = iv != null ? Arrays.hashCode(iv) : 0;
-            result = 31 * result + (pairs != null ? pairs.hashCode() : 0);
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "Entry{" +
-                    "iv=" + Hex.encodeHex(iv) +
-                    ", pairs=" + pairs +
-                    '}';
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        PiffSampleEncryptionBox that = (PiffSampleEncryptionBox) o;
-
-        if (algorithmId != that.algorithmId) return false;
-        if (ivSize != that.ivSize) return false;
-        if (!entries.equals(that.entries)) return false;
-        if (!Arrays.equals(kid, that.kid)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = algorithmId;
-        result = 31 * result + ivSize;
-        result = 31 * result + (kid != null ? Arrays.hashCode(kid) : 0);
-        result = 31 * result + entries.hashCode();
-        return result;
-    }
 }
