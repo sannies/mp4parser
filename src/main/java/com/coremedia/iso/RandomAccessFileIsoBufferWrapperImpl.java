0e64b55f01131c42f085b2653f9c1349ea8381bb
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-02-06 21:21:10 +0000

    buffered random access file
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@296 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/coremedia/iso/RandomAccessFileIsoBufferWrapperImpl.java
+++ src/main/java/com/coremedia/iso/RandomAccessFileIsoBufferWrapperImpl.java
@@ -11,17 +11,40 @@ import java.nio.channels.FileChannel;
  */
 public class RandomAccessFileIsoBufferWrapperImpl extends AbstractIsoBufferWrapper {
 
-    RandomAccessFile raf;
+    BufferedRandomAccessFile raf;
 
     public RandomAccessFileIsoBufferWrapperImpl(File file) throws IOException {
 
-        raf = new RandomAccessFile(file, "r");
+        raf = new BufferedRandomAccessFile(new RandomAccessFile(file, "r"));
+        //raf = new RandomAccessFile(file, "r");
 
 
     }
 
+    @Override
+    public long readUInt32() throws IOException {
+        byte[] c = new byte[4];
+        raf.readFully(c);
+        long uint32 = 0;
+        uint32 += (c[0] < 0 ? c[0] + 256 : c[0]) << 24;
+        uint32 += (c[1] < 0 ? c[1] + 256 : c[1]) << 16;
+        uint32 += (c[2] < 0 ? c[2] + 256 : c[2]) << 8;
+        uint32 += (c[3] < 0 ? c[3] + 256 : c[3]);
+        return uint32;
+    }
+
+    @Override
+    public int readUInt16() throws IOException {
+        byte[] c = new byte[2];
+        raf.readFully(c);
+        int uint16 = 0;
+        uint16 += (c[0] < 0 ? c[0] + 256 : c[0]) << 8;
+        uint16 += (c[1] < 0 ? c[1] + 256 : c[1]);
+        return uint16;
+    }
+
     public long position() throws IOException {
-        return raf.getChannel().position();
+        return raf.getFilePointer();
     }
 
     public void position(long position) throws IOException {
@@ -45,7 +68,7 @@ public class RandomAccessFileIsoBufferWrapperImpl extends AbstractIsoBufferWrapp
     }
 
     public long remaining() throws IOException {
-        return raf.length() - raf.getChannel().position();
+        return raf.length() - raf.getFilePointer();
     }
 
     public int read() throws IOException {
@@ -53,7 +76,8 @@ public class RandomAccessFileIsoBufferWrapperImpl extends AbstractIsoBufferWrapp
     }
 
     public int read(byte[] b) throws IOException {
-        return raf.read(b, 0, b.length);
+        raf.readFully(b, 0, b.length);
+        return b.length;
     }
 
     public long skip(long n) throws IOException {
