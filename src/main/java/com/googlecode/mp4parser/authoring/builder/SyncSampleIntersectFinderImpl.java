fb409bb6f54562f7b54d75ccefd674fb97f7bf7d
hoemmagnus@gmail.com <hoemmagnus@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-03-30 09:53:38 +0000

    Fixed how the sync samples are calculated.
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@448 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/authoring/builder/SyncSampleIntersectFinderImpl.java
+++ src/main/java/com/googlecode/mp4parser/authoring/builder/SyncSampleIntersectFinderImpl.java
@@ -47,19 +47,15 @@ public class SyncSampleIntersectFinderImpl implements FragmentIntersectionFinder
         }
 
         int[] chunkSizes = new int[syncSamples.length];
-        if (track.getSamples().size() == 1) {
-            chunkSizes[0] = 0;
-            for (int i = 1; i < chunkSizes.length; i++) {
-                chunkSizes[i] = 1;
-            }
-        } else {
-            long sc = track.getSamples().size();
-            double stretch = (double) sc / syncSampleContainingTrackSampleCount;
-            for (int i = 0; i < chunkSizes.length; i++) {
-                int start = (int) Math.round(stretch * (syncSamples[i] - 1));
-                chunkSizes[i] = start;
-                // The Stretch makes sure that there are as much audio and video chunks!
-            }
+        long sc = track.getSamples().size();
+        double stretch = (double) sc / syncSampleContainingTrackSampleCount;
+        chunkSizes[0] = 0;
+        for (int i = 1; i < chunkSizes.length; i++) {
+            int start = (int) Math.ceil(stretch * (syncSamples[i] - 1));
+//            int round = (int) Math.round(stretch * (syncSamples[i] - 1));
+            chunkSizes[i] = start;
+//            chunkSizes[i] = round;
+            // The Stretch makes sure that there are as much audio and video chunks!
         }
         return chunkSizes;
 
