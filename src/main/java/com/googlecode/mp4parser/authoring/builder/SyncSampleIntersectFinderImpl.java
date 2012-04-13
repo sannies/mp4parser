860565488cf58865339d14fca5ff835df693a1ca
michael.stattmann@gmail.com <michael.stattmann@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-04-13 01:42:39 +0000

    enable audio tracks again
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@460 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/authoring/builder/SyncSampleIntersectFinderImpl.java
+++ src/main/java/com/googlecode/mp4parser/authoring/builder/SyncSampleIntersectFinderImpl.java
@@ -32,25 +32,46 @@ import static com.googlecode.mp4parser.util.Math.lcm;
  */
 public class SyncSampleIntersectFinderImpl implements FragmentIntersectionFinder {
     public long[] sampleNumbers(Track track, Movie movie) {
+        Track syncSampleContainingTrack = null;
+        int syncSampleContainingTrackSampleCount = 0;
 
-
+        long[] syncSamples = new long[0];
         List<long[]> times = new LinkedList<long[]>();
         for (Track currentTrack : movie.getTracks()) {
-            times.add(getTimes(movie, currentTrack));
+            long[] currentTrackSyncSamples = currentTrack.getSyncSamples();
+            if (currentTrackSyncSamples != null && currentTrackSyncSamples.length > 0) {
+                final long[] currentTrackTimes = getTimes(movie, currentTrack);
+                times.add(currentTrackTimes);
+                syncSamples = getCommonIndices(currentTrackSyncSamples, currentTrackTimes, times.toArray(new long[times.size()][]));
+                if (syncSamples.length < (currentTrackSyncSamples.length * 0.3)) {
+                    throw new RuntimeException("There are less than 30% of common sync samples in the given track." + currentTrack);
+                } else if (syncSamples.length < (currentTrackSyncSamples.length * 0.6)) {
+                    System.err.println("There are less than 60% of common sync samples in the given track. This is implausible but I'm ok to continue." + currentTrack);
+                } else if (syncSamples.length < currentTrackSyncSamples.length) {
+                    System.err.println("Common SyncSample positions vs. this tracks SyncSample positions: " + syncSamples.length + " vs. " + currentTrackSyncSamples.length + " Track " + currentTrack);
+                } else {
+                    syncSampleContainingTrack = currentTrack;
+                    syncSampleContainingTrackSampleCount = currentTrack.getSamples().size();
+                }
+            }
         }
 
-
-        long[] syncSamples = getCommonIndices(track.getSyncSamples(), getTimes(movie, track), times.toArray(new long[times.size()][]));
-        if (syncSamples.length < (track.getSyncSamples().length * 0.3)) {
-            throw new RuntimeException("There are less than 30% of common sync samples in the given files.");
-        } else if (syncSamples.length < (track.getSyncSamples().length * 0.6)) {
-            System.err.println("There are less than 60% of common sync samples in the given files. This is implausible but I'm ok to continue");
-        } else if (syncSamples.length < track.getSyncSamples().length) {
-            System.err.println("Common SyncSample positions vs. this tracks SyncSample positions: " + syncSamples.length + " vs. " + track.getSyncSamples().length);
+        if (syncSampleContainingTrack == null) {
+            throw new RuntimeException("There was no track containing a Sync Sample Box but the Sync Sample Box is required to determine the fragment size.");
         }
 
-
-        return syncSamples;
+        long[] chunkSizes = new long[syncSamples.length];
+        long sc = track.getSamples().size();
+        double stretch = (double) sc / syncSampleContainingTrackSampleCount;
+        chunkSizes[0] = 1;
+        for (int i = 1; i < chunkSizes.length; i++) {
+            int start = (int) Math.ceil(stretch * (syncSamples[i]));
+//            int round = (int) Math.round(stretch * (syncSamples[i] - 1));
+            chunkSizes[i] = start;
+//            chunkSizes[i] = round;
+            // The Stretch makes sure that there are as much audio and video chunks!
+        }
+        return chunkSizes;
 
     }
 
