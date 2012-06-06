de9d1ea4a3d006871aed35d7c0e17ac46c5011ca
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-06-06 10:37:26 +0000

    activated H264Test - obviously magnus repaired the test
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@660 7decde4b-c250-0410-a0da-51896bc88be6

--- src/test/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImplTest.java
+++ src/test/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImplTest.java
@@ -35,7 +35,6 @@ import java.util.Arrays;
 public class H264TrackImplTest {
 
     @Test
-    @Ignore
     public void freeze() throws IOException {
         Track t = new H264TrackImpl(new BufferedInputStream(AACTrackImplTest.class.getResourceAsStream("/com/googlecode/mp4parser/authoring/tracks/h264-sample.h264")));
         Movie m = new Movie();
@@ -43,9 +42,9 @@ public class H264TrackImplTest {
 
         DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
         IsoFile isoFile = mp4Builder.build(m);
-        FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4").getChannel();
-        isoFile.getBox(fc);
-        fc.close();
+//        FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4").getChannel();
+//        isoFile.getBox(fc);
+//        fc.close();
         IsoFile isoFileReference = new IsoFile(Channels.newChannel(AACTrackImplTest.class.getResourceAsStream("/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4")));
         BoxComparator.check(isoFile, isoFileReference, Arrays.asList("/moov[0]/mvhd[0]", "/moov[0]/trak[0]/tkhd[0]", "/moov[0]/trak[0]/mdia[0]/mdhd[0]"));
     }
