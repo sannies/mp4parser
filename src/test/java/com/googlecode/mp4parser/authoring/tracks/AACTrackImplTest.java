157b721f49fa34c1e657c7ba5a075e670d524340
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-07-06 09:19:19 +0000

    some boilerplate code for re-generating ref file in case of change
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@706 7decde4b-c250-0410-a0da-51896bc88be6

--- src/test/java/com/googlecode/mp4parser/authoring/tracks/AACTrackImplTest.java
+++ src/test/java/com/googlecode/mp4parser/authoring/tracks/AACTrackImplTest.java
@@ -42,6 +42,9 @@ public class AACTrackImplTest {
 
         DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
         IsoFile isoFile = mp4Builder.build(m);
+//        FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/aac-sample.mp4").getChannel();
+//        isoFile.getBox(fc);
+//        fc.close();
         IsoFile isoFileReference = new IsoFile(Channels.newChannel(AACTrackImplTest.class.getResourceAsStream("/com/googlecode/mp4parser/authoring/tracks/aac-sample.mp4")));
         BoxComparator.check(isoFile, isoFileReference, "/moov[0]/mvhd[0]", "/moov[0]/trak[0]/tkhd[0]", "/moov[0]/trak[0]/mdia[0]/mdhd[0]");
     }
--- src/test/java/com/googlecode/mp4parser/authoring/tracks/AC3TrackImplTest.java
+++ src/test/java/com/googlecode/mp4parser/authoring/tracks/AC3TrackImplTest.java
@@ -7,8 +7,10 @@ import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import org.junit.Test;
 
 import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.util.Arrays;
 
 public class AC3TrackImplTest {
@@ -20,6 +22,10 @@ public class AC3TrackImplTest {
 
         DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
         IsoFile isoFile = mp4Builder.build(m);
+
+//        FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/ac3-sample.mp4").getChannel();
+//        isoFile.getBox(fc);
+//        fc.close();
         IsoFile isoFileReference = new IsoFile(Channels.newChannel(AACTrackImplTest.class.getResourceAsStream("/com/googlecode/mp4parser/authoring/tracks/ac3-sample.mp4")));
         BoxComparator.check(isoFile, isoFileReference, "/moov[0]/mvhd[0]", "/moov[0]/trak[0]/tkhd[0]", "/moov[0]/trak[0]/mdia[0]/mdhd[0]");
     }
