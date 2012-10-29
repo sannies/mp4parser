c4909eed578b39174dfb30e3ce10df86d55d5d02
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-10-29 08:10:55 +0000

    corrected test
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@786 7decde4b-c250-0410-a0da-51896bc88be6

--- src/test/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImplTest.java
+++ src/test/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImplTest.java
@@ -19,15 +19,11 @@ import com.coremedia.iso.IsoFile;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.util.Arrays;
 
 /**
  * Simple test to make sure nothing breaks.
@@ -42,9 +38,11 @@ public class H264TrackImplTest {
 
         DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
         IsoFile isoFile = mp4Builder.build(m);
-//        FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4").getChannel();
-//        isoFile.getBox(fc);
-//        fc.close();
+        //     FileChannel fc = new FileOutputStream("/home/sannies/scm/svn/mp4parser/isoparser/src/test/resources/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4").getChannel();
+        //     isoFile.getBox(fc);
+        //     fc.close();
+
+
         IsoFile isoFileReference = new IsoFile(Channels.newChannel(AACTrackImplTest.class.getResourceAsStream("/com/googlecode/mp4parser/authoring/tracks/h264-sample.mp4")));
         BoxComparator.check(isoFile, isoFileReference, "/moov[0]/mvhd[0]", "/moov[0]/trak[0]/tkhd[0]", "/moov[0]/trak[0]/mdia[0]/mdhd[0]");
     }
