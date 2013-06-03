dead40eaaa073b29e9599313fe5e1cdd18d52c09
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-06-03 10:11:25 +0000

    dropping unnecessary casts latest jar plugin
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@887 7decde4b-c250-0410-a0da-51896bc88be6

--- src/test/java/com/coremedia/iso/boxes/MediaHeaderBoxTest.java
+++ src/test/java/com/coremedia/iso/boxes/MediaHeaderBoxTest.java
@@ -16,9 +16,9 @@ public class MediaHeaderBoxTest extends BoxWriteReadBase<MediaHeaderBox> {
 
     @Override
     public void setupProperties(Map<String, Object> addPropsHere, MediaHeaderBox box) {
-        addPropsHere.put("creationTime", new Date(1370253188000L) );
+        addPropsHere.put("creationTime", new Date(1370253188000L));
         addPropsHere.put("duration", (long)12423 );
-        addPropsHere.put("language", (String)"ger" );
+        addPropsHere.put("language", "ger");
         addPropsHere.put("modificationTime", new Date(1370253188000L) );
         addPropsHere.put("timescale", (long)24 );
     }
