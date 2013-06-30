30e319b1db18016f6d5ddb30f3337f22e93500e5
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-06-30 11:44:27 +0000

    allow timescale and ticks to be specified for H264 cosntructor
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@891 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImpl.java
+++ src/main/java/com/googlecode/mp4parser/authoring/tracks/H264TrackImpl.java
@@ -54,13 +54,35 @@ public class H264TrackImpl extends AbstractTrack {
     private SEIMessage seiMessage;
     int frameNrInGop = 0;
     private boolean determineFrameRate = true;
-    private String lang = "und";
+    private String lang = "eng";
 
-    public H264TrackImpl(FileChannel fc, String lang, long timescale) throws IOException {
+    /**
+     * Creates a new <code>Track</code> object from a raw H264 source (<code>FileChannel fc</code>).
+     *
+     * Whenever the timescale and frametick are set to negative value (e.g. -1) the H264TrackImpl
+     * tries to detect the frame rate.
+     *
+     * Typically values for <code>timescale</code> and <code>frametick</code> are:
+     * <ul>
+     *     <li>23.976 FPS: timescale = 24000; frametick = 1001</li>
+     *     <li>25 FPS: timescale = 25; frametick = 1</li>
+     *     <li>29.97 FPS: timescale = 30000; frametick = 1001</li>
+     *     <li>30 FPS: timescale = 30; frametick = 1</li>
+     * </ul>
+     *
+     * @param fc the source file of the H264 samples
+     * @param lang language of the movie (in doubt: use "eng")
+     * @param timescale number of time units (ticks) in one second
+     * @param frametick number of time units (ticks) that pass while showing exactly one frame
+     * @throws IOException in case of problems whiel reading from the <code>FileChannel</code>
+     */
+    public H264TrackImpl(FileChannel fc, String lang, long timescale, int frametick) throws IOException {
         this.lang = lang;
         this.timescale = timescale; //e.g. 23976
-        this.frametick = 1000;
-        this.determineFrameRate = false;
+        this.frametick = frametick;
+        if ((timescale > 0) && (frametick > 0)) {
+            this.determineFrameRate = false;
+        }
         parse(fc);
     }
 
