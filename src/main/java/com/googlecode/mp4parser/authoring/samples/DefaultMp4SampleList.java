2351575c7b29a47ae615ba5a06657571bf14fa01
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2014-06-15 11:50:31 +0000

    mem map chunk wise - not sample wise
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@1070 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/authoring/samples/DefaultMp4SampleList.java
+++ src/main/java/com/googlecode/mp4parser/authoring/samples/DefaultMp4SampleList.java
@@ -4,8 +4,10 @@ import com.coremedia.iso.boxes.*;
 import com.googlecode.mp4parser.authoring.Sample;
 import com.googlecode.mp4parser.authoring.SampleImpl;
 
+import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
 import java.util.AbstractList;
 import java.util.List;
 
@@ -17,9 +19,10 @@ import static com.googlecode.mp4parser.util.CastUtils.l2i;
 public class DefaultMp4SampleList extends AbstractList<Sample> {
     Container topLevel;
     TrackBox trackBox = null;
-    SoftReference<Sample>[] cache = null;
+    SoftReference<ByteBuffer>[] cache = null;
     int[] chunkNumsStartSampleNum;
     long[] chunkOffsets;
+    int[] chunkSizes;
     SampleSizeBox ssb;
 
     public DefaultMp4SampleList(long track, Container topLevel) {
@@ -36,10 +39,13 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
             throw new RuntimeException("This MP4 does not contain track " + track);
         }
         chunkOffsets = trackBox.getSampleTableBox().getChunkOffsetBox().getChunkOffsets();
-        cache = (SoftReference<Sample>[]) Array.newInstance(SoftReference.class, size());
+        chunkSizes = new int[chunkOffsets.length];
+
+        cache = (SoftReference<ByteBuffer>[]) Array.newInstance(SoftReference.class, chunkOffsets.length );
         ssb = trackBox.getSampleTableBox().getSampleSizeBox();
         List<SampleToChunkBox.Entry> s2chunkEntries = trackBox.getSampleTableBox().getSampleToChunkBox().getEntries();
         SampleToChunkBox.Entry[] entries = s2chunkEntries.toArray(new SampleToChunkBox.Entry[s2chunkEntries.size()]);
+
 
         int s2cIndex = 0;
         SampleToChunkBox.Entry next = entries[s2cIndex++];
@@ -97,6 +103,14 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
         } while ((currentSampleNo += currentSamplePerChunk) <= lastSampleNo);
         chunkNumsStartSampleNum[currentChunkNo] = Integer.MAX_VALUE;
 
+        currentChunkNo=0;
+        for (int i = 1; i<= ssb.getSampleCount(); i++)  {
+            if (i == chunkNumsStartSampleNum[currentChunkNo]) {
+                currentChunkNo++;
+            }
+            chunkSizes[currentChunkNo-1] += ssb.getSampleSizeAtIndex(i-1);
+        }
+
     }
 
 
@@ -130,11 +144,8 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
 
     @Override
     public Sample get(int index) {
-        if (index >= cache.length) {
+        if (index >= ssb.getSampleCount()) {
             throw new IndexOutOfBoundsException();
-        }
-        if ((cache[index] != null) && (cache[index].get() != null)) {
-            return cache[index].get();
         }
 
         int currentChunkNoZeroBased = getChunkForSample(index);
@@ -142,14 +153,25 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
         int currentSampleNo = chunkNumsStartSampleNum[currentChunkNoZeroBased];
 
         long offset = chunkOffsets[l2i(currentChunkNoZeroBased)];
+        ByteBuffer chunk = cache[l2i(currentChunkNoZeroBased)]!=null?cache[l2i(currentChunkNoZeroBased)].get():null;
+        if (chunk == null) {
 
+            try {
+                chunk = topLevel.getByteBuffer(offset, chunkSizes[l2i(currentChunkNoZeroBased)] );
+                cache[l2i(currentChunkNoZeroBased)] = new SoftReference<ByteBuffer>(chunk);
+            } catch (IOException e) {
+                throw new IndexOutOfBoundsException(e.getMessage());
+            }
+        }
+
+        int offsetWithinChunk = 0;
         while (currentSampleNo < index + 1) {
-            offset += ssb.getSampleSizeAtIndex((currentSampleNo++) - 1);
+            offsetWithinChunk  += ssb.getSampleSizeAtIndex((currentSampleNo++) - 1);
         }
         final long sampleSize = ssb.getSampleSizeAtIndex(currentSampleNo - 1);
-        SampleImpl sampleImpl = new SampleImpl(offset, sampleSize, topLevel);
-        cache[index] = new SoftReference<Sample>(sampleImpl);
-        return sampleImpl;
+
+        return new SampleImpl(offsetWithinChunk, sampleSize,
+                (ByteBuffer) ((ByteBuffer)chunk.position(offsetWithinChunk)).slice().limit(l2i(sampleSize)));
     }
 
     @Override
