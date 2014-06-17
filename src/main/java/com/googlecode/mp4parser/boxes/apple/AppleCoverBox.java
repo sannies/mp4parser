31ca3576d30f02d998fb728f740d29e732d02bc9
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2014-06-17 06:38:33 +0000

    don't use ImageIO since it's not supported on android
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@1075 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/boxes/apple/AppleCoverBox.java
+++ src/main/java/com/googlecode/mp4parser/boxes/apple/AppleCoverBox.java
@@ -1,114 +1,32 @@
 package com.googlecode.mp4parser.boxes.apple;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by Tobias Bley / UltraMixer on 04/25/2014.
  */
-public class AppleCoverBox extends AppleDataBox
-{
+public class AppleCoverBox extends AppleDataBox {
     private Logger logger = Logger.getLogger(getClass().getName());
-    private Image cover;
-    private Image origCover;
     private byte[] data;
 
-    public AppleCoverBox()
-    {
+    public AppleCoverBox() {
         super("covr", 1);
     }
 
-    public Image getCover()
-    {
-        return cover;
+    @Override
+    protected byte[] writeData() {
+        return data;
     }
 
     @Override
-    protected byte[] writeData()
-    {
-        if (origCover == cover) {
-            return data;
-        } else {
-            throw new RuntimeException("At the moment you can't change the cover");
-        }
+    protected void parseData(ByteBuffer data) {
+        this.data = data.array();
     }
 
     @Override
-    protected void parseData(ByteBuffer data)
-    {
-        InputStream is = null;
-        try
-        {
-            this.data = data.array();
-            is = new ByteBufferBackedInputStream(data);
-            this.cover = this.origCover =  ImageIO.read(is);
-
-        }
-        catch (Exception e)
-        {
-            logger.log(Level.WARNING, "", e);
-        }
-        finally
-        {
-            if (is != null)
-            {
-                try
-                {
-                    is.close();
-                }
-                catch (IOException e)
-                {
-                    logger.log(Level.WARNING, "", e);
-                }
-            }
-        }
-
-        //To change body of implemented methods use File | Settings | File Templates.
+    protected int getDataLength() {
+        return data.length;
     }
 
-    @Override
-    protected int getDataLength()
-    {
-        logger.info("not yet implemented");
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public class ByteBufferBackedInputStream extends InputStream
-    {
-
-        private final ByteBuffer buf;
-
-        public ByteBufferBackedInputStream(ByteBuffer buf)
-        {
-            // make a coy of the buffer
-            this.buf = buf.duplicate();
-        }
-
-        public int read() throws IOException
-        {
-            if (!buf.hasRemaining())
-            {
-                return -1;
-            }
-            return buf.get() & 0xFF;
-        }
-
-        public int read(byte[] bytes, int off, int len)
-                throws IOException
-        {
-            if (!buf.hasRemaining())
-            {
-                return -1;
-            }
-
-            len = Math.min(len, buf.remaining());
-            buf.get(bytes, off, len);
-            return len;
-        }
-    }
 }
