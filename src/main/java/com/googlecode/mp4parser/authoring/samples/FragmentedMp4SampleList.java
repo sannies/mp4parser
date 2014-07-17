9f87f4fd537ade4c1a671615f535b0efa9d2416e
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2014-07-17 07:51:00 +0000

    major speedup for large files
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/isoparser@1101 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/authoring/samples/FragmentedMp4SampleList.java
+++ src/main/java/com/googlecode/mp4parser/authoring/samples/FragmentedMp4SampleList.java
@@ -5,19 +5,15 @@ import com.coremedia.iso.boxes.Box;
 import com.coremedia.iso.boxes.Container;
 import com.coremedia.iso.boxes.MovieBox;
 import com.coremedia.iso.boxes.TrackBox;
-import com.coremedia.iso.boxes.fragment.MovieFragmentBox;
-import com.coremedia.iso.boxes.fragment.TrackExtendsBox;
-import com.coremedia.iso.boxes.fragment.TrackFragmentBox;
-import com.coremedia.iso.boxes.fragment.TrackRunBox;
+import com.coremedia.iso.boxes.fragment.*;
 import com.googlecode.mp4parser.authoring.Sample;
 import com.googlecode.mp4parser.authoring.SampleImpl;
 import com.googlecode.mp4parser.util.Path;
 
 import java.lang.ref.SoftReference;
 import java.lang.reflect.Array;
-import java.util.AbstractList;
-import java.util.LinkedList;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 import static com.googlecode.mp4parser.util.CastUtils.l2i;
 
@@ -31,6 +27,9 @@ public class FragmentedMp4SampleList extends AbstractList<Sample> {
     TrackExtendsBox trex = null;
     private SoftReference<Sample> sampleCache[];
     private List<TrackFragmentBox> allTrafs;
+    private Map<TrackRunBox, SoftReference<ByteBuffer>>
+            trunDataCache = new HashMap<TrackRunBox, SoftReference<ByteBuffer>>();
+    private int firstSamples[];
     private int size_ = -1;
 
     public FragmentedMp4SampleList(long track, Container topLevel, IsoFile... fragments) {
@@ -53,9 +52,10 @@ public class FragmentedMp4SampleList extends AbstractList<Sample> {
             }
         }
         sampleCache = (SoftReference<Sample>[]) Array.newInstance(SoftReference.class, size());
+        initAllFragments();
     }
 
-    private List<TrackFragmentBox> allFragments() {
+    private List<TrackFragmentBox> initAllFragments() {
         if (allTrafs != null) {
             return allTrafs;
         }
@@ -79,18 +79,25 @@ public class FragmentedMp4SampleList extends AbstractList<Sample> {
             }
         }
         allTrafs = trafs;
+        int firstSample = 1;
+        firstSamples = new int[allTrafs.size()];
+        for (int i = 0; i < allTrafs.size(); i++) {
+            firstSamples[i] = firstSample;
+            firstSample += getTrafSize(allTrafs.get(i));
+        }
         return trafs;
     }
 
     private int getTrafSize(TrackFragmentBox traf) {
-        List<Box> boxes= traf.getBoxes();
-        for (int i =0; i< boxes.size();i++) {
+        List<Box> boxes = traf.getBoxes();
+        int size = 0;
+        for (int i = 0; i < boxes.size(); i++) {
             Box b = boxes.get(i);
             if (b instanceof TrackRunBox) {
-                return l2i(((TrackRunBox)b).getSampleCount());
+                size += l2i(((TrackRunBox) b).getSampleCount());
             }
         }
-        return 0;
+        return size;
     }
 
     @Override
@@ -101,61 +108,68 @@ public class FragmentedMp4SampleList extends AbstractList<Sample> {
             return cachedSample;
         }
 
-        int currentIndex = 1;
+
         int targetIndex = index + 1;
-        for (TrackFragmentBox trackFragmentBox : allFragments()) {
-            int trafSize = getTrafSize(trackFragmentBox);
-            if (targetIndex >= currentIndex && targetIndex < currentIndex + trafSize) {
-                // we got the correct traf.
-                int sampleIndexWithInTraf = targetIndex - currentIndex;
-                MovieFragmentBox moof = ((MovieFragmentBox) trackFragmentBox.getParent());
-                TrackRunBox trun = trackFragmentBox.getBoxes(TrackRunBox.class).get(0);
-                long offset = 0;
-                if (trun.isDataOffsetPresent()) {
-                    offset += trun.getDataOffset();
-                }
-                List<TrackRunBox.Entry> trackRunEntries = trun.getEntries();
-                if (trackFragmentBox.getTrackFragmentHeaderBox().hasBaseDataOffset()) {
-                    offset += trackFragmentBox.getTrackFragmentHeaderBox().getBaseDataOffset();
-                } else {
-                    offset += moof.getOffset();
-                }
-
-
-                for (int i = 0; i < sampleIndexWithInTraf; i++) {
-                    if (trun.isSampleSizePresent()) {
-                        offset += trackRunEntries.get(i).getSampleSize();
-                    } else {
-                        if (trackFragmentBox.getTrackFragmentHeaderBox().hasDefaultSampleSize()) {
-                            offset += trackFragmentBox.getTrackFragmentHeaderBox().getDefaultSampleSize();
-                        } else {
-                            if (trex == null) {
-                                throw new RuntimeException("File doesn't contain trex box but track fragments aren't fully self contained. Cannot determine sample size.");
-                            }
-                            offset += trex.getDefaultSampleSize();
-                        }
-                    }
-                }
-                long sampleSize;
-                if (trun.isSampleSizePresent()) {
-                    sampleSize = trackRunEntries.get(sampleIndexWithInTraf).getSampleSize();
-                } else {
-                    if (trackFragmentBox.getTrackFragmentHeaderBox().hasDefaultSampleSize()) {
-                        sampleSize = trackFragmentBox.getTrackFragmentHeaderBox().getDefaultSampleSize();
-                    } else {
-                        if (trex == null) {
-                            throw new RuntimeException("File doesn't contain trex box but track fragments aren't fully self contained. Cannot determine sample size.");
-                        }
-                        sampleSize = trex.getDefaultSampleSize();
-                    }
-                }
-                final SampleImpl sample = new SampleImpl(offset, sampleSize, moof.getParent());
-                sampleCache[index] = new SoftReference<Sample>(sample);
-                return sample;
-            }
-            currentIndex += trafSize;
+        int j = firstSamples.length-1;
+        while (targetIndex -  firstSamples[j] < 0) {
+            j--;
         }
-        return null;
+        TrackFragmentBox trackFragmentBox  = allTrafs.get(j);
+        // we got the correct traf.
+        int sampleIndexWithInTraf = targetIndex - firstSamples[j];
+        MovieFragmentBox moof = ((MovieFragmentBox) trackFragmentBox.getParent());
+
+        List<TrackRunBox> truns = trackFragmentBox.getBoxes(TrackRunBox.class);
+        if (truns.size()!=1) {
+            throw new RuntimeException("Please make me work with more than one trun");
+        }
+        TrackRunBox trun = truns.get(0);
+        List<TrackRunBox.Entry> trackRunEntries = trun.getEntries();
+        TrackFragmentHeaderBox tfhd = trackFragmentBox.getTrackFragmentHeaderBox();
+
+        long offset = 0;
+        Container base;
+        if (tfhd.hasBaseDataOffset()) {
+            offset += tfhd.getBaseDataOffset();
+            base = moof.getParent();
+        } else {
+            base = moof;
+        }
+
+        if (trun.isDataOffsetPresent()) {
+            offset += trun.getDataOffset();
+        }
+
+        boolean sampleSizePresent = trun.isSampleSizePresent();
+        boolean hasDefaultSampleSize = tfhd.hasDefaultSampleSize();
+        long defaultSampleSize = 0;
+        if (!sampleSizePresent) {
+            if (hasDefaultSampleSize) {
+                defaultSampleSize  = tfhd.getDefaultSampleSize();
+            } else {
+                if (trex == null) {
+                    throw new RuntimeException("File doesn't contain trex box but track fragments aren't fully self contained. Cannot determine sample size.");
+                }
+                defaultSampleSize  = trex.getDefaultSampleSize();
+            }
+        }
+        for (int i = 0; i < sampleIndexWithInTraf; i++) {
+            if (sampleSizePresent) {
+                offset += trackRunEntries.get(i).getSampleSize();
+            } else {
+                offset += defaultSampleSize;
+            }
+        }
+        long sampleSize;
+        if (sampleSizePresent) {
+            sampleSize = trackRunEntries.get(sampleIndexWithInTraf).getSampleSize();
+        } else {
+            sampleSize = defaultSampleSize;
+        }
+        final SampleImpl sample = new SampleImpl(offset, sampleSize, base);
+        sampleCache[index] = new SoftReference<Sample>(sample);
+        return sample;
+
     }
 
     @Override
