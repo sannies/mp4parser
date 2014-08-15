94a316b05a95187cfacccc5d3d9483c06fbff809
Sebastian Annies <Sebastian.Annies@castlabs.com>
2014-08-15 12:13:12 +0200

    generic Path return values

--- isoparser/src/main/java/com/googlecode/mp4parser/util/Path.java
+++ isoparser/src/main/java/com/googlecode/mp4parser/util/Path.java
@@ -59,46 +59,44 @@ public class Path {
         }
     }
 
-    public static Box getPath(Box box, String path) {
-        List<? extends Box> all = getPaths(box, path, true);
+    public static <T extends Box> T getPath(Box box, String path) {
+        List<T> all = getPaths(box, path, true);
         return all.isEmpty() ? null : all.get(0);
     }
 
-    public static Box getPath(Container container, String path) {
-        List<? extends Box> all = getPaths(container, path, true);
+    public static <T extends Box> T  getPath(Container container, String path) {
+        List<T> all = getPaths(container, path, true);
         return all.isEmpty() ? null : all.get(0);
     }
 
-    public static Box getPath(AbstractContainerBox containerBox, String path) {
-        List<? extends Box> all = getPaths(containerBox, path, true);
+    public static <T extends Box> T  getPath(AbstractContainerBox containerBox, String path) {
+        List<T> all = getPaths(containerBox, path, true);
         return all.isEmpty() ? null : all.get(0);
     }
 
-    public static List<Box> getPaths(AbstractContainerBox containerBox, String path) {
-        return getPaths(containerBox, path, false);
-    }
 
-    public static List<Box> getPaths(Box box, String path) {
+    public static <T extends Box> List<T> getPaths(Box box, String path) {
         return getPaths(box, path, false);
     }
 
-    public static List<Box> getPaths(Container container, String path) {
+    public static <T extends Box> List<T> getPaths(Container container, String path) {
         return getPaths(container, path, false);
     }
 
-    private static List<Box> getPaths(AbstractContainerBox container, String path, boolean singleResult) {
+    private static <T extends Box> List<T> getPaths(AbstractContainerBox container, String path, boolean singleResult) {
         return getPaths((Object) container, path, singleResult);
     }
 
-    private static List<Box> getPaths(Container container, String path, boolean singleResult) {
+    private static <T extends Box> List<T> getPaths(Container container, String path, boolean singleResult) {
         return getPaths((Object) container, path, singleResult);
     }
 
-    private static List<Box> getPaths(Box box, String path, boolean singleResult) {
+    private static <T extends Box> List<T>  getPaths(Box box, String path, boolean singleResult) {
         return getPaths((Object) box, path, singleResult);
     }
 
-    private static List<Box> getPaths(Object thing, String path, boolean singleResult) {
+    @SuppressWarnings("unchecked")
+    private static <T extends Box> List<T>  getPaths(Object thing, String path, boolean singleResult) {
         if (path.startsWith("/")) {
             path = path.substring(1);
             while (thing instanceof Box) {
@@ -108,7 +106,7 @@ public class Path {
 
         if (path.length() == 0) {
             if (thing instanceof Box) {
-                return Collections.singletonList((Box) thing);
+                return Collections.singletonList((T) thing);
             } else {
                 throw new RuntimeException("Result of path expression seems to be the root container. This is not allowed!");
             }
@@ -128,7 +126,7 @@ public class Path {
                 String type = m.group(1);
                 if ("..".equals(type)) {
                     if (thing instanceof Box) {
-                        return getPaths(((Box) thing).getParent(), later, singleResult);
+                        return Path.getPaths(((Box) thing).getParent(), later, singleResult);
                     } else {
                         return Collections.emptyList();
                     }
@@ -140,7 +138,7 @@ public class Path {
                             String indexString = m.group(3);
                             index = Integer.parseInt(indexString);
                         }
-                        List<Box> children = new LinkedList<Box>();
+                        List<T> children = new LinkedList<T>();
                         int currentIndex = 0;
                         // I'm suspecting some Dalvik VM to create indexed loops from for-each loops
                         // using the iterator instead makes sure that this doesn't happen
@@ -150,7 +148,7 @@ public class Path {
                             Box box1 = iterator.next();
                             if (box1.getType().matches(type)) {
                                 if (index == -1 || index == currentIndex) {
-                                    children.addAll(getPaths(box1, later, singleResult));
+                                    children.addAll(Path.<T>getPaths(box1, later, singleResult));
                                 }
                                 currentIndex++;
                             }
