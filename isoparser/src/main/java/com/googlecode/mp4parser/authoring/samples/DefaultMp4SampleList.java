4a3318d91f05d08c110b0645b792fbb24f8d4510
Sebastian Annies <Sebastian.Annies@googlemail.com>
2014-11-11 15:36:35 +0200

    speed up defaultmp4sample in case of large chunks: reduce access complexity from  O(n) to O(1)

--- isoparser/src/main/java/com/googlecode/mp4parser/authoring/samples/DefaultMp4SampleList.java
+++ isoparser/src/main/java/com/googlecode/mp4parser/authoring/samples/DefaultMp4SampleList.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 import java.util.AbstractList;
 import java.util.List;
+import java.util.UUID;
 
 import static com.googlecode.mp4parser.util.CastUtils.l2i;
 
@@ -21,6 +22,7 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
     int[] chunkNumsStartSampleNum;
     long[] chunkOffsets;
     int[] chunkSizes;
+    int[][] chunkSampleSizeSums;
     SampleSizeBox ssb;
     int lastChunk = 0;
 
@@ -42,6 +44,7 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
         chunkSizes = new int[chunkOffsets.length];
 
         cache = new ByteBuffer[chunkOffsets.length];
+        chunkSampleSizeSums = new int[chunkOffsets.length][];
         ssb = trackBox.getSampleTableBox().getSampleSizeBox();
         List<SampleToChunkBox.Entry> s2chunkEntries = trackBox.getSampleTableBox().getSampleToChunkBox().getEntries();
         SampleToChunkBox.Entry[] entries = s2chunkEntries.toArray(new SampleToChunkBox.Entry[s2chunkEntries.size()]);
@@ -73,6 +76,7 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
                     nextFirstChunk = Long.MAX_VALUE;
                 }
             }
+            chunkSampleSizeSums[currentChunkNo-1] = new int[currentSamplePerChunk];
 
         } while ((currentSampleNo += currentSamplePerChunk) <= lastSampleNo);
         chunkNumsStartSampleNum = new int[currentChunkNo + 1];
@@ -104,11 +108,16 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
         chunkNumsStartSampleNum[currentChunkNo] = Integer.MAX_VALUE;
 
         currentChunkNo=0;
+        int sampleSum = 0;
         for (int i = 1; i<= ssb.getSampleCount(); i++)  {
             if (i == chunkNumsStartSampleNum[currentChunkNo]) {
                 currentChunkNo++;
+                sampleSum = 0;
+
             }
             chunkSizes[currentChunkNo-1] += ssb.getSampleSizeAtIndex(i-1);
+            chunkSampleSizeSums[currentChunkNo-1][i - chunkNumsStartSampleNum[currentChunkNo-1]] = sampleSum;
+            sampleSum +=ssb.getSampleSizeAtIndex(i-1);
         }
 
     }
@@ -161,11 +170,9 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
             }
         }
 
-        int offsetWithinChunk = 0;
-        while (currentSampleNo < index + 1) {
-            offsetWithinChunk  += ssb.getSampleSizeAtIndex((currentSampleNo++) - 1);
-        }
-        final long sampleSize = ssb.getSampleSizeAtIndex(currentSampleNo - 1);
+
+        int offsetWithinChunk = chunkSampleSizeSums[l2i(currentChunkNoZeroBased)][index - (currentSampleNo - 1)];
+        final long sampleSize = ssb.getSampleSizeAtIndex(index);
 
         final ByteBuffer finalChunk = chunk;
         final int finalOffsetWithinChunk = offsetWithinChunk;
@@ -196,4 +203,32 @@ public class DefaultMp4SampleList extends AbstractList<Sample> {
     }
 
 
+    private class Calc {
+        private int index;
+        private int currentSampleNo;
+        private int offsetWithinChunk;
+        private long sampleSize;
+
+        public Calc(int index, int currentSampleNo) {
+            this.index = index;
+            this.currentSampleNo = currentSampleNo;
+        }
+
+        public int getOffsetWithinChunk() {
+            return offsetWithinChunk;
+        }
+
+        public long getSampleSize() {
+            return sampleSize;
+        }
+
+        public Calc invoke() {
+            offsetWithinChunk = 0;
+            while (currentSampleNo < index + 1) {
+                offsetWithinChunk  += ssb.getSampleSizeAtIndex((currentSampleNo++) - 1);
+            }
+            sampleSize = ssb.getSampleSizeAtIndex(currentSampleNo - 1);
+            return this;
+        }
+    }
 }
